19db8c664eecaf1f6e5655a3c5d6658b17baf911
Arienangel <arienangelts@gmail.com>
2024-01-29 16:08:43 +0800

    fix import

--- CHClab/common.py
+++ CHClab/common.py
@@ -4,21 +4,21 @@ from typing import Literal, Union
 from zipfile import ZipFile
 
 import datatable as dt
+import matplotlib.colors
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import scipy.optimize
 import scipy.signal
-from matplotlib.colors import LinearSegmentedColormap
 from scipy.constants import physical_constants
 
-cmap = LinearSegmentedColormap('Cmap',
-                               segmentdata={
-                                   'red': [[0, 1, 1], [0.05, 0, 0], [0.1, 0, 0], [0.15, 1, 1], [0.3, 1, 1], [1, 1, 1]],
-                                   'green': [[0, 1, 1], [0.05, 0, 0], [0.1, 1, 1], [0.15, 1, 1], [0.3, 0, 0], [1, 0, 0]],
-                                   'blue': [[0, 1, 1], [0.05, 1, 1], [0.1, 0, 0], [0.15, 0, 0], [0.3, 0, 0], [1, 1, 1]]
-                               },
-                               N=256)
+cmap = matplotlib.colors.LinearSegmentedColormap('Cmap',
+                                                 segmentdata={
+                                                     'red': [[0, 1, 1], [0.05, 0, 0], [0.1, 0, 0], [0.15, 1, 1], [0.3, 1, 1], [1, 1, 1]],
+                                                     'green': [[0, 1, 1], [0.05, 0, 0], [0.1, 1, 1], [0.15, 1, 1], [0.3, 0, 0], [1, 0, 0]],
+                                                     'blue': [[0, 1, 1], [0.05, 1, 1], [0.1, 0, 0], [0.15, 0, 0], [0.3, 0, 0], [1, 1, 1]]
+                                                 },
+                                                 N=256)
 
 G0, *_ = physical_constants['conductance quantum']
 
